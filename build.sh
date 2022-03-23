dc0cd3448628de0e18130e21471ef5a00253cdb4
Orlando Oliveira <84464728+orlandosh@users.noreply.github.com>
2022-03-23 15:56:22 -0300

    feat: port build.sh to macOS & add .icns

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 function single() {
-    inkscape -h $1 whiskers.svg --export-filename whiskers_$1x$1.png
+    inkscape -h $1 whiskers.svg --export-filename whiskers.iconset/icon_$1x$1.png
 }
 
+mkdir whiskers.iconset
+
+single 16
 single 32
 single 64
 single 128
 single 256
 single 512
 single 1024
+
+iconutil --convert icns --output whiskers.icns whiskers.iconset
+
+mv whiskers.iconset/icon_*.png .
+rm -r whiskers.iconset

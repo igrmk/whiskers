1693088e80f5e0e0139e6ec5071cd71f2b9a5fcb
Igor Mikushkin <igor.mikushkin@gmail.com>
2022-12-26 02:09:29 +0000

    Build icons for mac, fit icons into squircles

--- README.md
+++ README.md
@@ -17,11 +17,22 @@ Installation
 
 ### macOS
 
-PRs are welcome.
+1. Find `kitty.app` in the `Applications` folder, select it and press `⌘ + i`.
+2. Drag `whiskers.icns` onto the application icon in the `kitty info` pane.
+3. Delete the icon cache and restart `Dock`:
+
+```bash
+$ rm /var/folders/*/*/*/com.apple.dock.iconcache; killall Dock
+```
 
 ### Windows
 
 PRs are welcome.
+
+Development
+-----------
+
+You need mac to build icons. Install inkscape and exiftool. Then run `build.sh` script.
 
 Other alternatives
 ------------------
--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-function single() {
-    inkscape -h $1 whiskers.svg --export-filename whiskers_$1x$1.png
-}
+set -euo pipefail
 
-single 32
-single 64
-single 128
-single 256
-single 512
-single 1024
+tmp="$(mktemp -d)"
+mac_icons="$tmp/mac.icons"
+mac_iconset="$tmp/mac.iconset"
+mkdir "$mac_icons"
+mkdir "$mac_iconset"
+
+for i in 16 32 64 128 256 512 1024
+do
+    inkscape -h $i whiskers.svg --export-area=-18.3:-18.3:274.3:274.3 --export-filename whiskers_${i}x${i}.png
+    inkscape -h $i whiskers.svg --export-area=-31.1:-31.1:287.1:287.1 --export-filename "$mac_icons/whiskers_${i}x${i}.png"
+    exiftool -all= -overwrite_original_in_place whiskers_${i}x${i}.png
+    exiftool -all= -overwrite_original_in_place "$mac_icons/whiskers_${i}x${i}.png"
+done
+
+for i in 16 32 128 256 512
+do
+    cp "$mac_icons/whiskers_${i}x${i}.png" "$mac_iconset/icon_${i}x${i}.png"
+    cp "$mac_icons/whiskers_$((i * 2))x$((i * 2)).png" "$mac_iconset/icon_${i}x${i}@2x.png"
+done
+
+iconutil --convert icns --output whiskers.icns "$mac_iconset"
+rm -r "$tmp"
